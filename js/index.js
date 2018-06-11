3f7e5346a4e4998cbff455e2b4934c1eb4806d1c
AFew <1367291468@qq.com>
2018-06-11 13:56:33 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,81 +1,82 @@
-$(function() {
-	//回到顶部
-	$('#goToTop').hide();
-	$(window).scroll(function() {
-		if($(this).scrollTop() > 800) {
-			$('#goToTop').fadeIn();
-		} else {
-			$('#goToTop').fadeOut();
-		}
-	});
-	$('#goToTop a').click(function() {
-		$('html ,body').animate({
-			scrollTop: 0
-		}, 600);
-		return false;
-	});
-	//下拉菜单
-	$('#nav li').hover(function() {
-		$('ul', this).stop().slideDown(200);
-		$(this).children('a:first').addClass("hov");
-	}, function() {
-		$('ul', this).stop().slideUp(100);
-		$(this).children('a:first').removeClass("hov");
-	});
-	//显示二维码
-	$('#erweimawx').hover(
-		function() {
-			$('#erweimawx').css("cursor", "pointer");
-			$('.erweima_wx').stop().fadeIn();
-		},
-		function() {
-			$('.erweima_wx').stop().fadeOut();
-		});
+$(function () {
 	//轮播图
 	var count = 0;
 	var t;
 	lunbo();
-	$(".arrow-right").click(function() {
+	$(".arrow-right").click(function () {
 		clearInterval(t);
 		count++;
-		if(count == $(".slider>ul>li").length) {
+		if (count == $(".slider>ul>li").length) {
 			count = 0;
 		}
 		yingChan(count);
 		lunbo();
 	})
 
-	$(".arrow-left").click(function() {
+	$(".arrow-left").click(function () {
 		clearInterval(t);
 		count--;
-		if(count == -1) {
+		if (count == -1) {
 			count = $(".slider>ul>li").length - 1;
 		}
 		yingChan(count);
 		lunbo();
 	})
-	$(".round>ul>li").mouseenter(function() {
+	$(".round>ul>li").mouseenter(function () {
 		clearInterval(t);
 		var index = $(this).index();
 		yingChan(index);
 		count = index;
 	})
-	$(".round>ul>li").mouseleave(function() {
+	$(".round>ul>li").mouseleave(function () {
 		lunbo();
 	})
 
 	function yingChan(a) {
-		$(".slider>ul>li").eq(a).fadeIn().siblings("li").fadeOut();
+		$(".slider>ul>li").eq(a).stop().fadeIn().siblings("li").stop().fadeOut();
 		$(".round>ul>li").eq(a).addClass("roundLiHover").siblings("li").removeClass("roundLiHover");
 	}
 
 	function lunbo() {
-		t = setInterval(function() {
+		t = setInterval(function () {
 			count++;
-			if(count == $(".slider>ul>li").length) {
+			if (count == $(".slider>ul>li").length) {
 				count = 0;
 			}
 			yingChan(count);
-		}, 3000);
+		}, 5000);
 	}
+	//回到顶部
+	$('#goToTop').hide();
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 800) {
+			$('#goToTop').fadeIn();
+		} else {
+			$('#goToTop').fadeOut();
+		}
+	});
+	$('#goToTop a').click(function () {
+		$('html ,body').animate({
+			scrollTop: 0
+		}, 600);
+		return false;
+	});
+	//下拉菜单
+	$('#nav li').hover(function () {
+		$('ul', this).stop().slideDown(200);
+		$(this).children('a:first').addClass("hov");
+	}, function () {
+		$('ul', this).stop().slideUp(100);
+		$(this).children('a:first').removeClass("hov");
+	});
+	//显示二维码
+	$('#erweimawx').hover(function () {
+		$('#erweimawx').css("cursor", "pointer");
+		$('.erweima_wx').stop().fadeIn();
+	}, function () {
+		$('.erweima_wx').stop().fadeOut();
+	});
+	//设置轮播图圆点居中
+		var round = $('.round').innerWidth();
+		$('.round').css("marginLeft", -(round / 2));
 });
