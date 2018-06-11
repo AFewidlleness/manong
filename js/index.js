3741b6b60b83fc60dc61042c842d7f0f633b7a4b
AFew <1367291468@qq.com>
2018-06-11 11:39:26 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,18 +1,6 @@
-//下拉菜单
-$(document).ready(function() {
-	$('#nav li').hover(function() {
-		$('ul', this).stop().slideDown(200);
-		$(this).children('a:first').addClass("hov");
-	}, function() {
-		$('ul', this).stop().slideUp(100);
-		$(this).children('a:first').removeClass("hov");
-	});
-});
-
-
-//回到顶部
 $(function() {
-	$('#goToTop').hide(); 
+	//回到顶部
+	$('#goToTop').hide();
 	$(window).scroll(function() {
 		if($(this).scrollTop() > 800) {
 			$('#goToTop').fadeIn();
@@ -26,25 +14,68 @@ $(function() {
 		}, 600);
 		return false;
 	});
-
-});
-
-//显示二维码
-$(document).ready(function(){
+	//下拉菜单
+	$('#nav li').hover(function() {
+		$('ul', this).stop().slideDown(200);
+		$(this).children('a:first').addClass("hov");
+	}, function() {
+		$('ul', this).stop().slideUp(100);
+		$(this).children('a:first').removeClass("hov");
+	});
+	//显示二维码
 	$('#erweimawx').hover(
-		function (){
-			$('#erweimawx').css("cursor","pointer");
-			$('.erweima_wx').fadeIn();
-	},function (){
-		$('.erweima_wx').fadeOut();
-	});
-});
+		function() {
+			$('#erweimawx').css("cursor", "pointer");
+			$('.erweima_wx').stop().fadeIn();
+		},
+		function() {
+			$('.erweima_wx').stop().fadeOut();
+		});
+	//轮播图
+	var count = 0;
+	var t;
+	lunbo();
+	$(".arrow-right").click(function() {
+		clearInterval(t);
+		count++;
+		if(count == $(".slider>ul>li").length) {
+			count = 0;
+		}
+		yingChan(count);
+		lunbo();
+	})
 
+	$(".arrow-left").click(function() {
+		clearInterval(t);
+		count--;
+		if(count == -1) {
+			count = $(".slider>ul>li").length - 1;
+		}
+		yingChan(count);
+		lunbo();
+	})
+	$(".round>ul>li").mouseenter(function() {
+		clearInterval(t);
+		var index = $(this).index();
+		yingChan(index);
+		count = index;
+	})
+	$(".round>ul>li").mouseleave(function() {
+		lunbo();
+	})
 
-$(document).ready(function (e) {
-	$('#b03').unslider({
-		dots: true, //是否显示指示器，取值true / false
-		delay:3500, //图片停留的时间，数字，单位为毫秒
-		speed:800   //图片切换的速度，数字，单位为毫秒
-	});
+	function yingChan(a) {
+		$(".slider>ul>li").eq(a).fadeIn().siblings("li").fadeOut();
+		$(".round>ul>li").eq(a).addClass("roundLiHover").siblings("li").removeClass("roundLiHover");
+	}
+
+	function lunbo() {
+		t = setInterval(function() {
+			count++;
+			if(count == $(".slider>ul>li").length) {
+				count = 0;
+			}
+			yingChan(count);
+		}, 3000);
+	}
 });
